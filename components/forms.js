5a2b40c45589a3cc668808fe0e4ce0cbeda76975
medhavi <medhavi.srivastava16@gmail.com>
2020-10-01 00:22:44 +0530

    email and password textinput created

--- components/forms.js
+++ components/forms.js
@@ -42,6 +42,7 @@ const styles = StyleSheet.create({
         backgroundColor: colors.lightGrey,
         borderColor: colors.primary,
         color: colors.black,
+        
         },
       
   });
