6d8b873cb8d8bb795be7c32eba62d860412e1fa0
medhavi <medhavi.srivastava16@gmail.com>
2020-10-02 05:32:34 +0530

    created signup screen

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LoginScreen/>
+      <SignupScreen/>
     </View>
   );
 }
