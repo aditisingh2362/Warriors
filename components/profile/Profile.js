018cd9d3faad9e7237c5e46a4d5fa80c17db42f1
tomarviii88 <tomarviii88@gmail.com>
2020-10-05 11:25:59 +0530

    removed comment in DoctorComponent

---
+++ components/profile/Profile.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import Constant from 'expo-constants';
+import { colors } from '../constants/theme';
+
+const Profile = () => {
+  return <View></View>;
+};
+
+export default Profile;
