0baf8c7bbecbb5e437edbfd0c7c4c5d6330fa52b
medhavi <medhavi.srivastava16@gmail.com>
2020-10-11 23:30:22 +0530

    integrated firebase

---
+++ constants/firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB4LrZs0eTfnhLshFrDcOnoDodREWEgvhs",
+  authDomain: "warriors-84004.firebaseapp.com",
+  databaseURL: "https://warriors-84004.firebaseio.com",
+  projectId: "warriors-84004",
+  storageBucket: "warriors-84004.appspot.com",
+  // messagingSenderId: "sender-id",
+  // appId: "app-id",
+  // measurementId: "G-measurement-id"
+};
+
+firebase.initializeApp(firebaseConfig);
